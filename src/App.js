5e883290e00534c455863b503401e0dc228d6a82
ravikisha <ravikishan63392@gmail.com>
2022-06-08 02:25:38 +0530

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './components/navbar/navbar.js';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Home from './components/pages/homepage/homepage';
+import Contact from './components/pages/contactpage/contactpage';
+import Pricing from './components/pages/pricing/pricing';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Navbar />
+     <Routes>
+       <Route path="/" exact element={<Home />} />
+       <Route path="/conference" element={<div>Conference</div>} />
+        <Route path="/about_gwalior" element={<div>About Gwalior</div>} />
+        <Route path="/guidelines" element={<div>Guidelines</div>} />
+        <Route path="/schedule" element={<div>Schedule</div>} />
+        <Route path="/important_dates" element={<div>Important Dates</div>} />
+        <Route path="/advisory_memebers" element={<div>Advisory Memebers</div>} />
+        <Route path="/registration" element={<Pricing />} />
+        <Route path="/contact" element={<Contact />} />
+     </Routes>
+    </Router>
   );
 }
 
